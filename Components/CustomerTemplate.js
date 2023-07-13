fbb30fd9072dcd594b150ff1c5afff49d5c52ec8
Ann Shen <yazu0419@gmail.com>
2023-07-13 13:24:46 +0800

    added handleValidateFood

--- Components/CustomerTemplate.js
+++ Components/CustomerTemplate.js
@@ -1,8 +1,8 @@
 import { Box, Center, Circle, Image, Text } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import foodList from "../contents/foodList";
-import { sample } from "lodash";
+import { isEqual, sample } from "lodash";
 
 const statusColor = {
   eating: "#EDDDD6",
@@ -16,6 +16,7 @@ const CustomerTemplate = ({ id, src }) => {
   const status = data[id].status;
   const wishFood = data[id].order ?? "";
   const isCoffee = wishFood === "coffee";
+  const [overTime, setOverTime] = useState();
 
   useEffect(() => {
     const controlTime = (s, time) => {
@@ -30,12 +31,35 @@ const CustomerTemplate = ({ id, src }) => {
     controlTime("eating", 5000);
   }, [status]);
 
+  useEffect(() => {
+    if (!overTime) {
+      const t = setTimeout(() => {
+        setOverTime(true);
+      }, [10000]);
+      return () => clearTimeout(t);
+    }
+  }, [overTime]);
+
+  const handleValidateFood = () => {
+    if (data[id].order.includes("&")) {
+      const checkContent = (s) => {
+        return s?.split("&");
+      };
+      return isEqual(
+        checkContent(data.targetItem).sort(),
+        checkContent(data[id].order).sort()
+      );
+    } else {
+      return data[id].order === data.targetItem;
+    }
+  };
   return (
     <Box
+      userSelect="none"
       onDrop={(e) => {
         e.preventDefault();
         const key = `plateContent${data?.targetPlate}`;
-        if (data[id].order === data.targetItem) {
+        if (handleValidateFood()) {
           setValue(`${id}.status`, "eating");
           setValue(key, []);
           const controlNextOrder = () => {
@@ -44,6 +68,7 @@ const CustomerTemplate = ({ id, src }) => {
             }, [5000]);
             return () => clearTimeout(t);
           };
+          setOverTime(false);
           controlNextOrder();
         } else {
           if (status === "eating") return;
@@ -55,15 +80,22 @@ const CustomerTemplate = ({ id, src }) => {
       onDragOver={(e) => {
         e.preventDefault();
         e.stopPropagation();
-      }}>
+      }}
+    >
       <Center
         visibility={status !== "eating" ? "visible" : "hidden"}
         w={isCoffee ? "3em" : "7em"}
-        h='4em'>
-        <Image src={`/${wishFood}.svg`} w='100%' zIndex={2} />
+        h="4em"
+      >
+        <Image src={`/${wishFood}.svg`} w="100%" zIndex={2} />
       </Center>
-      <Circle bg={statusColor[status]} w='10em' h='10em' pos='relative'>
-        <Image top={3} pos='absolute' src={`${src}.svg`} w='10em'></Image>
+
+      <Circle bg={statusColor[status]} w="10em" h="10em" pos="relative">
+        {overTime ? (
+          <Image top={1} pos="absolute" src={`${src}-angry.svg`} w="9.5em" />
+        ) : (
+          <Image top={0} pos="absolute" src={`${src}.svg`} w="9.5em" />
+        )}
       </Circle>
     </Box>
   );
