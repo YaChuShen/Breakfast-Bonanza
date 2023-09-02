a37fc3eac8585ce2ffad2f0a70384323905b07ab
ann-shen <yazu0419@gmail.com>
2023-09-02 14:05:41 +0800

    wip

---
+++ pages/register.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Login from "../Components/Login";
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+
+const register = () => {
+  return (
+    <Box w='100%' h='3em' pos='fixed' top={0} zIndex={1}>
+      <Login session={session} />
+    </Box>
+  );
+};
+
+export default register;
