1712308fdc742726312d97918ae9e5a7877140d5
ann-shen <yazu0419@gmail.com>
2023-12-03 23:14:47 +0800

    wip

--- Components/FoodPlateSection.js
+++ Components/FoodPlateSection.js
@@ -6,7 +6,7 @@ const FoodPlateSection = () => {
   return (
     <HStack pos='relative' spacing={1}>
       <Box pos='absolute' w='10em' left={-5} bottom={-10}>
-        <Image src='/foodPlate.svg' />
+        <Image src='/foodPlate.svg' alt='' />
       </Box>
       <FoodTemplate value={"egg"} src={"egg"} w='3em' zIndex={1} />
       <FoodTemplate value={"hotDog0"} src={"hotDog0"} w='4.5em' zIndex={1} />
