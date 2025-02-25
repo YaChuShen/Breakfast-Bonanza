05c62ecfc3992559ed57bb50ed6c74c85499798e
ann-shen <yazu0419@gmail.com>
2025-02-25 16:06:53 +0800

    chore: update Firebase configuration to use environment variables

--- firebase.config.js
+++ firebase.config.js
@@ -8,18 +8,16 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDWnM_sJAN6F7dpplV24coorpV0O35ioaU',
-  authDomain: 'breakfast-cooking-game.firebaseapp.com',
-  projectId: 'breakfast-cooking-game',
-  storageBucket: 'gs://breakfast-cooking-game.appspot.com',
-  messagingSenderId: '228402986359',
-  appId: '1:228402986359:web:a569c9b702f056c7fab572',
-  measurementId: 'G-QPCD8PVT2P',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
